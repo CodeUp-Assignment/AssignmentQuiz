79322c93d49b3ecc2036fe53a8de7ffea49d5c5e
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-10 00:49:09 +0530

    Update Question.java

--- Question.java
+++ Question.java
@@ -6,6 +6,7 @@
  * 
  * Date of Creation : 08/10/2024
  */
+
 public class Question {
     private static int questionCount = 0; 
     private static Question[] questionBank = new Question[10]; 
@@ -14,143 +15,93 @@ public class Question {
     private String[] options = new String[4];
     private String answer;
 
-    //Initializes a new instance of the Question class with the specified question text, answer options, and correct answer.    
+    // Initializes a new instance of the Question class with the specified question text, answer options, and correct answer.  
     public Question(String questionText, String[] options, String answer) {
-        this.id = ++questionCount; 
+        this.id = ++questionCount;
         this.questionText = questionText;
         this.options = options;
         this.answer = answer;
-    }
 
-    //Takes a question text, an array of answer options, and the correct answer as arguments. 
-    //It creates a new Question object and adds it to the question bank. If the bank is full, it expands its size.
-    public static void addQuestionBank(String questionText, String[] options, String answer) {
-        if (questionCount >= questionBank.length) {
-            resizeQuestionBank();
-        }
-        questionBank[questionCount] = new Question(questionText, options, answer);
-        System.out.println("Question added with ID: " + questionCount);
-    }
-
-    //Accepts an ID as an argument and returns the Question object that matches the given ID. 
-   //If no match is found, it returns null.
-    public static Question getQuestionById(int id) {
-        for (int i = 0; i < questionCount; i++) {
-            if (questionBank[i].id == id) {
-                return questionBank[i];
-            }
-        }
-        return null; 
-    }
-
-    //Displays all questions in the question bank. If there are no questions, it informs the user.
-    public static void displayQuestions() {
-        if (questionCount == 0) {
-            System.out.println("No questions available in the question bank.");
-        } else {
-            for (int i = 0; i < questionCount; i++) {
-                System.out.println("ID: " + questionBank[i].id + " - " + questionBank[i].questionText);
-            }
+        // Add the question to the bank if there is space
+        if (questionCount <= questionBank.length) {
+            questionBank[questionCount - 1] = this; 
         }
     }
 
-    //A private method that expands the size of the questionBank array when it is full.
-    private static void resizeQuestionBank() {
-        Question[] newBank = new Question[questionBank.length * 2]; 
-        System.arraycopy(questionBank, 0, newBank, 0, questionBank.length);
-        questionBank = newBank;
+    /** 
+     * Returns the unique identifier for the question
+     * @return the ID of the question
+     */
+    public int getId() {
+        return id;
     }
 
-    //Getter for returning the text of the question
+    /** 
+     * Returns the text of the question.
+     * @return the question text
+     */
     public String getQuestionText() {
         return questionText;
     }
 
-    //Getters for returning the correct answer for the question
+    /** 
+     * Returns the option at the specified index.
+     */
+    public String getOption(int index) {
+        if (index >= 0 && index < options.length) {
+            return options[index];
+        }
+        return null; // Or throw an exception
+    }
+
+    /** 
+     * Returns the correct answer for the question.
+     */
     public String getAnswer() {
         return answer;
     }
 
-    //Getter for returning the total number of questions currently in the question bank
-    public static int getTotalQuestions() {
-        return questionCount;
-    }
-}
-/***
- * This class represents a question in a quiz, including its text, answer options, and the correct answer. 
- * It also maintains a static question bank to store multiple questions.
- * 
- * Owner : Avadhi-Singhal
- * 
- * Date of Creation : 08/10/2024
- */
-public class Question {
-    private static int questionCount = 0; 
-    private static Question[] questionBank = new Question[10]; 
-    private final int id; 
-    private String questionText;
-    private String[] options = new String[4];
-    private String answer;
-
-    //Initializes a new instance of the Question class with the specified question text, answer options, and correct answer.    
-    public Question(String questionText, String[] options, String answer) {
-        this.id = ++questionCount; 
-        this.questionText = questionText;
-        this.options = options;
-        this.answer = answer;
-    }
-
-    //Takes a question text, an array of answer options, and the correct answer as arguments. 
-    //It creates a new Question object and adds it to the question bank. If the bank is full, it expands its size.
-    public static void addQuestionBank(String questionText, String[] options, String answer) {
-        if (questionCount >= questionBank.length) {
-            resizeQuestionBank();
+    /** 
+     * Displays all questions in the question bank.
+     */
+    public static void displayQuestions() {
+        if (questionCount == 0) {
+            System.out.println("No questions available.");
+            return;
         }
-        questionBank[questionCount] = new Question(questionText, options, answer);
-        System.out.println("Question added with ID: " + questionCount);
+        for (int i = 0; i < questionCount; i++) {
+            Question q = questionBank[i];
+            System.out.println(q.id + ": " + q.getQuestionText());
+            for (int j = 0; j < 4; j++) {
+                System.out.println("  " + (j + 1) + ") " + q.getOption(j));
+            }
+        }
     }
 
-    //Accepts an ID as an argument and returns the Question object that matches the given ID. 
-   //If no match is found, it returns null.
+    /** 
+     * Returns the total number of questions in the question bank.
+     */
+    public static int getTotalQuestions() {
+        return questionCount; // Returns the total number of questions added
+    }
+
+    /** 
+     * Retrieves a question from the question bank by its ID.
+     * @return the question if found, or null if not found
+     */
     public static Question getQuestionById(int id) {
         for (int i = 0; i < questionCount; i++) {
-            if (questionBank[i].id == id) {
+            if (questionBank[i].getId() == id) {
                 return questionBank[i];
             }
         }
-        return null; 
+        return null; // If not found
     }
 
-    //Displays all questions in the question bank. If there are no questions, it informs the user.
-    public static void displayQuestions() {
-        if (questionCount == 0) {
-            System.out.println("No questions available in the question bank.");
-        } else {
-            for (int i = 0; i < questionCount; i++) {
-                System.out.println("ID: " + questionBank[i].id + " - " + questionBank[i].questionText);
-            }
-        }
-    }
-
-    //A private method that expands the size of the questionBank array when it is full.
-    private static void resizeQuestionBank() {
-        Question[] newBank = new Question[questionBank.length * 2]; 
-        System.arraycopy(questionBank, 0, newBank, 0, questionBank.length);
-        questionBank = newBank;
-    }
-
-    //Getter for returning the text of the question
-    public String getQuestionText() {
-        return questionText;
-    }
-
-    //Getters for returning the correct answer for the question
-    public String getAnswer() {
-        return answer;
-    }
-
-    //Getter for returning the total number of questions currently in the question bank
-    public static int getTotalQuestions() {
-        return questionCount;
+    /** 
+     * Adds a new question to the question bank.
+     */
+    public static void addQuestionBank(String questionText, String[] options, String answer) {
+        new Question(questionText, options, answer); // Automatically adds to the question bank
     }
 }
