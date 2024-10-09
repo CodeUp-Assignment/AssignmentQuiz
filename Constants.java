cb0e519c957de5636357fb275ceabce6fddb3470
Pratul Agarwal <146626154+Pratula98@users.noreply.github.com>
2024-10-09 16:52:40 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,15 @@
+package assignment_java;
+
+//Constant class to manage hardcoded values
+ public class Constants {
+public static final String BINARY = "binary";
+
+
+public static final String INPUTTYPE = "Enter the type of input number system:\n" +
+	    "1. Binary\n" +
+	    "2. Octal\n" +
+	    "3. Decimal\n" +
+	    "4. Hexadecimal" ;
+
+}
+
