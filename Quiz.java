6d7ed8f2f2b5b19b142515736aa60126fbc3bace
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-09 12:29:09 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,57 +1,34 @@
-
-//public class Quiz {
-//	public String title = "";
-//	public int maxQuestion = 0;
-//	
-//	public Quiz() {
-//	
-//	}
-//	
-//	public Quiz(String title, int maxQuestion) {
-//		this.title = title;
-//		this.maxQuestion = maxQuestion;
-//	}
-//}
-
-
 public class Quiz {
     private String title;
-    private int maxQuestions;
-    private Question[] questions;  
-    private int currentQuestionCount = 0;  
-    
-    // Constructor to initialize quiz title and maximum questions
-    public Quiz(String title, int maxQuestions) {
+    public Question[] questions;
+    private int currentQuestionCount = 0;
+
+    public Quiz(String title) {
         this.title = title;
-        this.maxQuestions = maxQuestions;
-        questions = new Question[maxQuestions];  
+        this.questions = new Question[5];
     }
 
-    // Method to add a question to the quiz
-    public boolean addQuestion(Question newQuestion) {
-        // Check if the quiz is already full
-        if (currentQuestionCount >= maxQuestions) {
-            System.out.println("Cannot add more questions. Maximum limit reached.");
-            return false;
-        }
-        for (int i = 0; i < currentQuestionCount; i++) {
-            if (questions[i].getQuestionText().equalsIgnoreCase(newQuestion.getQuestionText())) {
-                System.out.println("Duplicate question found. Cannot add the same question twice.");
-                return false;
-            }
+    public void addQuestion(Question newQuestion) {
+        if (currentQuestionCount >= questions.length) {
+            resizeQuestionsArray();
         }
         questions[currentQuestionCount] = newQuestion;
         currentQuestionCount++;
-        return true;
     }
 
-    // Method to display all questions
+    private void resizeQuestionsArray() {
+        Question[] newQuestions = new Question[questions.length * 2];
+        for (int i = 0; i < questions.length; i++) {
+            newQuestions[i] = questions[i];
+        }
+        questions = newQuestions;
+    }
+
     public void displayQuiz() {
         System.out.println("Quiz Title: " + title);
         for (int i = 0; i < currentQuestionCount; i++) {
-            questions[i].displayQuestion(i + 1);  
+            questions[i].displayQuestion(i + 1);
             System.out.println("------------------------");
         }
     }
 }
-
