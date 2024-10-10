bb6f900bb9c6e48358b8e2079f9e5cb4773a6854
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-10 09:02:51 +0530

    Update DefaultQuiz.java

--- DefaultQuiz.java
+++ DefaultQuiz.java
@@ -11,28 +11,57 @@ package assignment_java_programming;
 
 public class DefaultQuiz extends Quiz {
 
+	   private Question[] questions; 
+	    private int questionCount;
     /** 
      * Constructor for the DefaultQuiz class, initializing the quiz with a predefined title and loading the default questions.
      */
     public DefaultQuiz() {
         super("Default Java Quiz"); 
-        defaultQuestions(); 
-    }
+        questions = new Question[Constants.DEFAULT_QUESTIONS.length]; 
+        questionCount = 0; 
+        defaultQuestions();     }
 
     /** 
      * Initializes the default questions, options, and answers for the quiz.
      * It creates Question objects and adds them to the quiz.
      */
     private void defaultQuestions() {
-        String[] questions = Constants.DEFAULT_QUESTIONS ;
-        String[][] options = Constants.DEFAULT_OPTIONS ;
-
-        String[] answers = Constants.DEFAULT_ANSWERS;
-
-        // Loop through each question and create a Question object
-        for (int i = 0; i < questions.length; i++) {
-            Question question = new Question(questions[i], options[i], answers[i]); 
+        String[] questionsData = Constants.DEFAULT_QUESTIONS;
+        String[][] optionsData = Constants.DEFAULT_OPTIONS;
+        String[] answersData = Constants.DEFAULT_ANSWERS;
+        for (int i = 0; i < questionsData.length; i++) {
+            Question question = new Question(questionsData[i], optionsData[i], answersData[i]); 
             addQuestion(question);  
         }
     }
+    
+    /**
+     * This method is use to add questions to the array
+     */
+    public void addQuestion(Question question) {
+        if (questionCount < questions.length) { 
+            questions[questionCount] = question; 
+            questionCount++; 
+        } else {
+            System.out.println("Cannot add more questions, quiz is full.");
+        }
+    }
+    
+    
+    public Question getQuestion(int index) {
+        if (index >= 0 && index < questionCount) {
+            return questions[index]; 
+        }
+        return null;
+    }
+    
+    public int getQuestionCount() {
+        return questionCount; 
+    }
+    
+    public String getTitle() {
+        return super.getTitle(); 
+    }
+
 }
