764dd7f02b0b13d755fdfbbbb3e55c49c6bc7e9c
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 11:40:11 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -17,17 +17,6 @@ public class Quiz {
     // Constructor for quiz with a title
     public Quiz(String title) {
         this.title = title;
-        this.questions = new Question[5];
-        this.currentQuestionCount = 0;
-    }
-
-    // Adds a question to the quiz
-    public void addQuestion(Question question) {
-        if (currentQuestionCount >= questions.length) {
-            resizeQuestionsArray();
-        }
-        questions[currentQuestionCount++] = question;
-        System.out.println("Question added to quiz: " + question.getQuestionText());
     }
 
     // Displays the quiz details
@@ -36,14 +25,5 @@ public class Quiz {
         for (int i = 0; i < currentQuestionCount; i++) {
             System.out.println("ID: " + questions[i].getId() + " - " + questions[i].getQuestionText());
         }
-    }
-
-    // Resizes the questions array
-    private void resizeQuestionsArray() {
-        Question[] newQuestions = new Question[questions.length * 2];
-        for (int i = 0; i < questions.length; i++) {
-            newQuestions[i] = questions[i];
-        }
-        questions = newQuestions;
     }
 }
