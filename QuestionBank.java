b798f4ae459eb3357d1b471b5d2481590504c2e6
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 11:41:00 +0530

    Update QuestionBank.java

--- QuestionBank.java
+++ QuestionBank.java
@@ -10,31 +10,36 @@ package assignment_java_programming;
  */
 
 public class QuestionBank {
-    private Question[] questions;
+    public Question[] question;
     private int questionCount;
 
     public QuestionBank() {
-        this.questions = new Question[10]; 
+        this.question = new Question[10]; 
         this.questionCount = 0;
     }
 
     // Adds a new question to the question bank. Expands the array if needed.
     public void addQuestion(String questionText, String[] options, String answer) {
-        if (questionCount >= questions.length) {
+        if (questionCount >= question.length) {
             resizeQuestionBank();
         }
-        questions[questionCount++] = new Question(questionCount, questionText, options, answer);
+        question[questionCount++] = new Question(questionCount, questionText, options, answer);
         System.out.println("Question added with ID: " + questionCount);
     }
 
     // Retrieves a question by ID.
     public Question getQuestionById(int id) {
         for (int i = 0; i < questionCount; i++) {
-            if (questions[i].getId() == id) {
-                return questions[i];
+            if (question[i].getId() == id) {
+                return question[i];
             }
         }
         return null;
+    }
+
+    // Retrieves the current question count.
+    public int getQuestionCount() {
+        return questionCount;
     }
 
     // Displays all questions in the question bank.
@@ -43,17 +48,17 @@ public class QuestionBank {
             System.out.println("No questions available in the question bank.");
         } else {
             for (int i = 0; i < questionCount; i++) {
-                System.out.println("ID: " + questions[i].getId() + " - " + questions[i].getQuestionText());
+                System.out.println("ID: " + question[i].getId() + " - " + question[i].getQuestionText());
             }
         }
     }
 
     // Resizes the question bank array when needed.
     private void resizeQuestionBank() {
-        Question[] newQuestions = new Question[questions.length * 2];
-        for (int i = 0; i < questions.length; i++) {
-            newQuestions[i] = questions[i];
+        Question[] newQuestions = new Question[question.length * 2];
+        for (int i = 0; i < question.length; i++) {
+            newQuestions[i] = question[i];
         }
-        questions = newQuestions;
+        question = newQuestions;
     }
 }
