6a0892dc9714db7fe4c72b19c65d820454b649d8
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 14:25:19 +0530

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,78 +1,70 @@
 /***
- * This class represents a question in a quiz, including its text, answer options, and the correct answer. 
- * It also maintains a static question bank to store multiple questions.
+ * The Question class represents an individual question in a quiz, including its text, answer options,
+ * and correct answer. Each Question object has a unique ID that can be used to reference it in a question 
+ * bank or quiz.
  * 
  * Owner : Avadhi-Singhal
  * 
- * Date of Creation : 08/10/2024
+ * Date of Creation: 21/10/2024
  */
-public class Question {
-    private static int questionCount = 0; 
-    private static Question[] questionBank = new Question[10]; 
-    private final int id; 
-    private String questionText;
-    private String[] options = new String[4];
-    private String answer;
 
-    //Initializes a new instance of the Question class with the specified question text, answer options, and correct answer.    
-    public Question(String questionText, String[] options, String answer) {
-        this.id = ++questionCount; 
+public class Question {
+    private final int id; 
+    private String questionText; 
+    private String[] options; 
+    private String answer; 
+
+    /**
+     * Constructor for initializing the question with its details.
+     * 
+     * @param id          The unique identifier for the question.
+     * @param questionText The text of the question.
+     * @param options      The possible options for the question.
+     * @param answer       The correct answer.
+     */
+    public Question(int id, String questionText, String[] options, String answer) {
+        this.id = id;
         this.questionText = questionText;
-        this.options = options;
+        this.options = new String[options.length]; 
+        for (int i = 0; i < options.length; i++) {
+            this.options[i] = options[i]; 
+        }
         this.answer = answer;
     }
 
-    //Takes a question text, an array of answer options, and the correct answer as arguments. 
-    //It creates a new Question object and adds it to the question bank. If the bank is full, it expands its size.
-    public static void addQuestionBank(String questionText, String[] options, String answer) {
-        if (questionCount >= questionBank.length) {
-            resizeQuestionBank();
-        }
-        questionBank[questionCount] = new Question(questionText, options, answer);
-        System.out.println("Question added with ID: " + questionCount);
+    /**
+     * Gets the unique identifier of the question.
+     * 
+     * @return The ID of the question.
+     */
+    public int getId() {
+        return id;
     }
 
-    //Accepts an ID as an argument and returns the Question object that matches the given ID. 
-   //If no match is found, it returns null.
-    public static Question getQuestionById(int id) {
-        for (int i = 0; i < questionCount; i++) {
-            if (questionBank[i].id == id) {
-                return questionBank[i];
-            }
-        }
-        return null; 
-    }
-
-    //Displays all questions in the question bank. If there are no questions, it informs the user.
-    public static void displayQuestions() {
-        if (questionCount == 0) {
-            System.out.println("No questions available in the question bank.");
-        } else {
-            for (int i = 0; i < questionCount; i++) {
-                System.out.println("ID: " + questionBank[i].id + " - " + questionBank[i].questionText);
-            }
-        }
-    }
-
-    //A private method that expands the size of the questionBank array when it is full.
-    private static void resizeQuestionBank() {
-        Question[] newBank = new Question[questionBank.length * 2]; 
-        System.arraycopy(questionBank, 0, newBank, 0, questionBank.length);
-        questionBank = newBank;
-    }
-
-    //Getter for returning the text of the question
+    /**
+     * Gets the text of the question.
+     * 
+     * @return The question text.
+     */
     public String getQuestionText() {
         return questionText;
     }
 
-    //Getters for returning the correct answer for the question
+    /**
+     * Gets the correct answer of the question.
+     * 
+     * @return The correct answer.
+     */
     public String getAnswer() {
         return answer;
     }
 
-    //Getter for returning the total number of questions currently in the question bank
-    public static int getTotalQuestions() {
-        return questionCount;
+    /**
+     * Gets the possible options for the question.
+     * 
+     * @return An array of options.
+     */
+    public String[] getOptions() {
+        return options;
     }
 }
