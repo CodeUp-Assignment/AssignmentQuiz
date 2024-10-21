2e8638b570b72b76cec495cc670f8096f20a7799
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 10:00:55 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,80 +1,49 @@
 package assignment_java_programming;
 
-/**
- * The Quiz class represents a quiz with a title and a collection of questions.
- * It provides methods for adding questions and displaying quiz information.
+/***
+ * The Quiz class represents a quiz containing a title and a collection of questions. It provides functionality 
+ * to add questions to the quiz and display the quiz content.
  * 
- * Creator : Avadhi-Singhal
+ * Owner : Avadhi-Singhal
  * 
- * Date of Creation : 08/10/2024
+ * Date of Creation : 10/10/2024
  */
 
 public class Quiz {
-    private String title; 
-    private Question[] questions = new Question[5]; 
-    private int currentQuestionCount = 0; 
-    /** 
-     * Constructs a Quiz object with a specified title.
-     */
+    private String title;
+    private Question[] questions;
+    private int currentQuestionCount;
+
+    // Constructor for quiz with a title
     public Quiz(String title) {
         this.title = title;
+        this.questions = new Question[5];
+        this.currentQuestionCount = 0;
     }
 
-    /** 
-     * Adds a question to the quiz.
-     * If the current question count exceeds the capacity of the questions array, it resizes the array.
-     */
+    // Adds a question to the quiz
     public void addQuestion(Question question) {
         if (currentQuestionCount >= questions.length) {
-            resizeQuestionsArray(); 
+            resizeQuestionsArray();
         }
         questions[currentQuestionCount++] = question;
         System.out.println("Question added to quiz: " + question.getQuestionText());
     }
 
-    /** 
-     * Resizes the questions array to add more questions by doubling its size.
-     */
-    private void resizeQuestionsArray() {
-        Question[] newQuestions = new Question[questions.length * 2];
-        for (int i = 0; i < questions.length; i++) {
-            newQuestions[i] = questions[i]; 
-        }
-        questions = newQuestions; 
-    }
-
-    /** 
-     * @return the number of questions in the quiz
-     */
-    public int getQuestionCount() {
-        return currentQuestionCount; 
-    }
-
-    /** 
-     * @return the title of the quiz
-     */
-    public String getTitle() {
-        return title; 
-    }
-
-    /** 
-     * Retrieves a question by its index.
-     * @return the Question object at the specified index, or null if the index is invalid
-     */
-    public Question getQuestion(int index) {
-        if (index >= 0 && index < currentQuestionCount) {
-            return questions[index]; 
-        }
-        return null; 
-    }
-
-    /** 
-     * Displays the title of the quiz and all its questions.
-     */
+    // Displays the quiz details
     public void displayQuiz() {
         System.out.println("Quiz Title: " + title);
         for (int i = 0; i < currentQuestionCount; i++) {
-            System.out.println("ID: " + questions[i].getQuestionText()); 
+            System.out.println("ID: " + questions[i].getId() + " - " + questions[i].getQuestionText());
         }
+    }
+
+    // Resizes the questions array
+    private void resizeQuestionsArray() {
+        Question[] newQuestions = new Question[questions.length * 2];
+        for (int i = 0; i < questions.length; i++) {
+            newQuestions[i] = questions[i];
+        }
+        questions = newQuestions;
     }
 }
