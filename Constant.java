001c8b72f646e0dca5163380928e4662eeda78a3
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 14:22:38 +0530

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,36 @@
+/***
+ * This is the constant file for quizathon program
+ * 
+ * Owner : Avadhi-Singhal
+ * 
+ * Date of Creation : 21/10/2024
+ */
+public class Constant {
+	  public String EXIT_MESSAGE = "Exiting...";
+	  public String INVALID_CHOICE =  "Invalid choice. Please try again.";
+    public String EMPTY_QUESTION_BANK_MSG = "No questions available to add to the quiz. Returning to main menu.";
+    public String ADD_QUESTION_BY_ID = "Enter the question ID to add to the quiz (or type 'done' to finish): ";
+    public String DONE = "done";
+    public String ADD_QUESTION = "Adding question: ";
+    public String INVALID_QUESTION_ID = "Invalid question ID.";
+    public String PROMPT_VALID_INTEGER = "Please enter a valid number or 'done' to finish.";
+    public String MULTIPLE_QUESTION_MESSAGE = "You can add multiple questions. Type 'done' when you are finished.";
+    public String ENTER_OPTION = "Enter option ";
+    public String CONSTRAINT_MSG = " (type 'done' to finish adding options, cannot be empty and must be less than 100 characters):";
+    public int DEFAULT_OPTIONS_SIZE = 15;
+    public int DEFAULT_QUESTION_NUMBERS = 10;
+    public int MAXIMUM_CHARACTERS = 100;
+    public String EMPTY_OPTION_MSG = "Option cannot be empty and must be less than 100 characters. Please try again.";
+    public String DUPLICATE_OPTION_MSG = "Duplicate option detected. Please enter a different option.";
+    public String QUIZ_TITLE = "Quiz Title: ";
+    public String ID = "ID: ";
+    public String HYPHEN = " - ";
+    public String QUESTION_ADDED_MSG = "Question added with ID: ";
+    public String NO_QUESTION_AVAILABLE = "No questions available in the question bank.";
+    public String CHOOSE_OPTION = "Choose an option: \n1. Create quiz \n2. Add questions \n3. Exit";
+    public String ENTER_CHOICE = "Enter your choice (1-3): ";
+    public String INVALID_NUMBER = "Please enter a number between 1 and 3.";
+    public String PROMPT_VALID_CHOICE = "Invalid input. Please enter a number between 1 and 3.";
+    public String PROMPT_TITLE = "Enter the title of the quiz (cannot be empty and must be less than 100 characters): ";
+    public String PROMPT_QUESTION_TEXT = "Enter the question text (cannot be empty and must be less than 100 characters):";
+}
