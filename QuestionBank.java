74d506eb3072048fb3badeaa9d3e0aa416b538d3
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 14:17:48 +0530

    Update QuestionBank.java

--- QuestionBank.java
+++ QuestionBank.java
@@ -10,55 +10,79 @@ package assignment_java_programming;
  */
 
 public class QuestionBank {
-    public Question[] question;
-    private int questionCount;
+	Constant constant = new Constant(); 
+	
+    private Question[] questions; 
+    private int questionCount; 
 
+    /**
+     * Constructor for initializing the question bank with a default size.
+     */
     public QuestionBank() {
-        this.question = new Question[10]; 
+        this.questions = new Question[constant.DEFAULT_QUESTION_NUMBERS]; 
         this.questionCount = 0;
     }
 
-    // Adds a new question to the question bank. Expands the array if needed.
+    /**
+     * Adds a new question to the question bank, resizing if necessary.
+     * 
+     * @param questionText The text of the question to add.
+     * @param options      The possible options for the question.
+     * @param answer       The correct answer.
+     */
     public void addQuestion(String questionText, String[] options, String answer) {
-        if (questionCount >= question.length) {
-            resizeQuestionBank();
+        if (questionCount >= questions.length) {
+            resizeQuestionBank(); 
         }
-        question[questionCount++] = new Question(questionCount, questionText, options, answer);
-        System.out.println("Question added with ID: " + questionCount);
+        questions[questionCount++] = new Question(questionCount, questionText, options, answer); 
+        System.out.println(constant.QUESTION_ADDED_MSG  + questionCount);
     }
 
-    // Retrieves a question by ID.
+    /**
+     * Retrieves a question by its unique identifier.
+     * 
+     * @param id The ID of the question to retrieve.
+     * @return The question if found, otherwise null.
+     */
     public Question getQuestionById(int id) {
         for (int i = 0; i < questionCount; i++) {
-            if (question[i].getId() == id) {
-                return question[i];
+            if (questions[i].getId() == id) {
+                return questions[i]; 
             }
         }
-        return null;
+        return null; 
     }
 
-    // Retrieves the current question count.
+    /**
+     * Gets the current count of questions in the question bank.
+     * 
+     * @return The current question count.
+     */
     public int getQuestionCount() {
         return questionCount;
     }
 
-    // Displays all questions in the question bank.
+    /**
+     * Displays all questions in the question bank.
+     */
     public void displayQuestions() {
         if (questionCount == 0) {
-            System.out.println("No questions available in the question bank.");
+            System.out.println(constant.NO_QUESTION_AVAILABLE);
         } else {
             for (int i = 0; i < questionCount; i++) {
-                System.out.println("ID: " + question[i].getId() + " - " + question[i].getQuestionText());
+                System.out.println(constant.ID + questions[i].getId() + constant.HYPHEN + questions[i].getQuestionText());
             }
         }
     }
 
-    // Resizes the question bank array when needed.
+    /**
+     * Resizes the question bank array when needed.
+     */
     private void resizeQuestionBank() {
-        Question[] newQuestions = new Question[question.length * 2];
-        for (int i = 0; i < question.length; i++) {
-            newQuestions[i] = question[i];
+        Question[] newQuestions = new Question[questions.length * 2]; 
+        for (int i = 0; i < questions.length; i++) {
+            newQuestions[i] = questions[i]; 
         }
-        question = newQuestions;
+        questions = newQuestions; 
     }
 }
