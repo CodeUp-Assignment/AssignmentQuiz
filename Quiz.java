c912994f84cb232fec91558280ed005a1e224fec
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-10-21 14:16:57 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,8 +1,7 @@
 package assignment_java_programming;
 
 /***
- * The Quiz class represents a quiz containing a title and a collection of questions. It provides functionality 
- * to add questions to the quiz and display the quiz content.
+ * The Quiz class represents a quiz with a title and a collection of questions.
  * 
  * Owner : Avadhi-Singhal
  * 
@@ -10,20 +9,53 @@ package assignment_java_programming;
  */
 
 public class Quiz {
-    private String title;
-    private Question[] questions;
-    private int currentQuestionCount;
-
-    // Constructor for quiz with a title
+	Constant constant = new Constant();
+	
+    private String title; 
+    private Question[] questions; 
+    private int currentQuestionCount; 
+    
+    /**
+     * Constructor for initializing the quiz with a title.
+     * 
+     * @param title The title of the quiz.
+     */
     public Quiz(String title) {
         this.title = title;
+        this.questions = new Question[constant.DEFAULT_QUESTION_NUMBERS]; 
+        this.currentQuestionCount = 0;
     }
 
-    // Displays the quiz details
+    /**
+     * Displays the quiz details, including the title and questions.
+     */
     public void displayQuiz() {
-        System.out.println("Quiz Title: " + title);
+        System.out.println(constant.QUIZ_TITLE + title);
         for (int i = 0; i < currentQuestionCount; i++) {
-            System.out.println("ID: " + questions[i].getId() + " - " + questions[i].getQuestionText());
+            System.out.println(constant.ID + questions[i].getId() + constant.HYPHEN + questions[i].getQuestionText());
         }
+    }
+
+    /**
+     * Adds a question to the quiz.
+     * 
+     * @param question The question to be added.
+     */
+    public void addQuestion(Question question) {
+        if (currentQuestionCount >= questions.length) {
+            resizeQuiz();
+        }
+        questions[currentQuestionCount++] = question; 
+    }
+
+    /**
+     * Resizes the question array to accommodate more questions.
+     */
+    private void resizeQuiz() {
+        Question[] newQuestions = new Question[questions.length * 2]; 
+        for (int i = 0; i < questions.length; i++) {
+            newQuestions[i] = questions[i]; 
+        }
+        questions = newQuestions; 
     }
 }
