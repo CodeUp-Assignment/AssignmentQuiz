2f67f74dc0bbaf3934e5bd5c7c309f28bc31f5aa
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-22 12:14:36 +0530

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,4 +1,4 @@
-package assignment_java_programming;
+package practice.java;
 /***
  * The Question class represents an individual question in a quiz, including its text, answer options,
  * and correct answer. Each Question object has a unique ID that can be used to reference it in a question 
@@ -10,38 +10,61 @@ package assignment_java_programming;
  */
 
 public class Question {
-    private final int id;
-    private String questionText;
-    private String[] options;
-    private String answer;
+    private final int id; 
+    private String questionText; 
+    private String[] options; 
+    private String answer; 
 
-    // Constructor for initializing the question
+    /**
+     * Constructor for initializing the question with its details.
+     * 
+     * @param id          The unique identifier for the question.
+     * @param questionText The text of the question.
+     * @param options      The possible options for the question.
+     * @param answer       The correct answer.
+     */
     public Question(int id, String questionText, String[] options, String answer) {
         this.id = id;
         this.questionText = questionText;
-        this.options = new String[options.length];
+        this.options = new String[options.length]; 
         for (int i = 0; i < options.length; i++) {
-            this.options[i] = options[i];
+            this.options[i] = options[i]; 
         }
         this.answer = answer;
     }
 
-    // Getter for ID
+    /**
+     * Gets the unique identifier of the question.
+     * 
+     * @return The ID of the question.
+     */
     public int getId() {
         return id;
     }
 
-    // Getter for question text
+    /**
+     * Gets the text of the question.
+     * 
+     * @return The question text.
+     */
     public String getQuestionText() {
         return questionText;
     }
 
-    // Getter for answer
+    /**
+     * Gets the correct answer of the question.
+     * 
+     * @return The correct answer.
+     */
     public String getAnswer() {
         return answer;
     }
 
-    // Getter for options
+    /**
+     * Gets the possible options for the question.
+     * 
+     * @return An array of options.
+     */
     public String[] getOptions() {
         return options;
     }
