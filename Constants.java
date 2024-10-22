bb40874032270bab9003fca19f9503603890e513
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-22 12:14:59 +0530

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,14 +1,15 @@
+package practice.java;
 /**
  * This class contains a set of constant string that are used throughout the program to display messages. 
  * Owner-->Pratul Agarwal
  * Date of creation-->09/09/2024
  */
-package assignment_java_programming;
+
 public final class Constants {
 	public static final String NAME = "Enter your name: ";
 	public static final String E_MAIL = "Enter your email: ";
 			
-	 public static final String USER_CHOICE ="Wait A Minute! Who are you...?\n" +
+	 public static final String USER_CHOICE ="Do you want to create a quiz or attempt a quiz\n" +
 			 "1. Creator\n2. Participant\n"  +
 			 "Enter 1 for Creator\n"  +
 			 "Enter 2 for Participant\n" +
@@ -26,78 +27,48 @@ public final class Constants {
     public static final String ANSWER = "Enter the correct answer (must be one of the options and cannot be empty):";
       
     public static final String QUESTION_ID = "Enter the question ID to add to the quiz (or type 'done' to finish): ";
-    
-    public static final String NO_QUESTIONS = "No questions available to add to the quiz. Returning to main menu.";
-        
-    public static final String QUIZ_TITLE =  "Enter the title of the quiz (cannot be empty and must be less than 100 characters): ";
-    
-    public static final String NO_QUIZ = "There is no Quiz created yet.";
-    
+         
+    public static final String TITLE =  "Enter the title of the quiz (cannot be empty and must be less than 100 characters): ";
+       
     public static final String PARTICIPANT_CHOICE = "For Answer - Select from 1-4 \n" +
-    		"For Skip - Click Enter \n" +
-    		"For Previous Question - Write j \n" +
-    		"For Next Question - Write l \n" +
-    		"For Ending a Quiz and submitting your answer - Write k";
+    		"For Skip - Write -  \n" +
+    		"For Previous Question - Write p \n" +
+    		"For Next Question - Write n \n" +
+    		"For Ending a Quiz and submitting your answer - Write s";
      
-    public static final String[] DEFAULT_QUESTIONS =  {
-            "What is the keyword used to create a class in Java?",
-            "Which data type is used to create a variable that should store text?",
-            "What is the correct way to create an object in Java?",
-            "Which method can be used to find the length of a string in Java?",
-            "What is the output of 10 + 20 + \"30\" in Java?"
-        };
-    
-    public static final String[][] DEFAULT_OPTIONS = {
-            {"class", "Class", "CLASS", "All of the above"},
-            {"String", "String[]", "char", "int"},
-            {"new ClassName()", "ClassName new()", "new Class()", "Class new()"},
-            {"length()", "size()", "getLength()", "length"},
-            {"102030", "30", "10", "Error"}
-        };
-    
-    public static final String[] DEFAULT_ANSWERS = {
-            "All of the above",
-            "String",
-            "new ClassName()",
-            "length()",
-            "102030"
-        };  
-  
-    public static final String EXIT = "Exiting program...";
-    
-    public static final String PARTICIPANT = "Participant ";
-    
-    public static final String PERFORMING_QUIZ = " is performing an quiz.";
-    
-    public static final String START_QUIZ = "Let's start the quiz : ";
-    
-    public static final String AVAILABLE_QUIZ = "Available quizzes: ";
-    
-    public static final String PLACEHOLDER = "Placeholder";
-    
-    public static final String EMAIL_ID ="placeholder@mail.com";
-
     public static final String START_QUIT_QUIZ = "Type 1 to play the default quiz. Type 'exit' to leave.";
-    
-    public static final String EXIT_QUIZ = "exit";
-    
-    public static final String WELCOME = "Welcome to the Quiz : ";
-    
-    public static final String FIRST_QUESTION = "Already at First Question";
-    
-    public static final String LAST_QUESTION = "Already at Last Question";
-    
-    public static final String QUESTION_NO = "Question ";
-    
-    public static final String NOT_AVAILABLE = " is not available.";
-
+   
     public static final String USERS_CHOICE = "Enter your Choice : ";
 
-    public static final String FINISH ="Quiz Finished!";
+   
+    public String EXIT_MESSAGE = "Exiting...";
+	  public static final String INVALID_CHOICE =  "Invalid choice. Please try again.";
+  public String EMPTY_QUESTION_BANK_MSG = "No questions available to add to the quiz. Returning to main menu.";
+  public String ADD_QUESTION_BY_ID = "Enter the question ID to add to the quiz (or type 'done' to finish): ";
+  public String DONE = "done";
+  public String ADD_QUESTION = "Adding question: ";
+  public String INVALID_QUESTION_ID = "Invalid question ID.";
+  public String PROMPT_VALID_INTEGER = "Please enter a valid number or 'done' to finish.";
+  public String MULTIPLE_QUESTION_MESSAGE = "You can add multiple questions. Type 'done' when you are finished.";
+  public String ENTER_OPTION = "Enter option ";
+  public String CONSTRAINT_MSG = " (type 'done' to finish adding options, cannot be empty and must be less than 100 characters):";
+  public int DEFAULT_OPTIONS_SIZE = 15;
+  public int DEFAULT_QUESTION_NUMBERS = 10;
+  public int MAXIMUM_CHARACTERS = 100;
+  public String EMPTY_OPTION_MSG = "Option cannot be empty and must be less than 100 characters. Please try again.";
+  public String DUPLICATE_OPTION_MSG = "Duplicate option detected. Please enter a different option.";
+  public String QUIZ_TITLE = "Quiz Title: ";
+  public String ID = "ID: ";
+  public String HYPHEN = " - ";
+  public String QUESTION_ADDED_MSG = "Question added with ID: ";
+  public String NO_QUESTION_AVAILABLE = "No questions available in the question bank.";
+  public String CHOOSE_OPTION = "Choose an option: \n1. Create quiz \n2. Add questions \n3. Exit";
+  public String ENTER_CHOICE = "Enter your choice (1-3): ";
+  public String INVALID_NUMBER = "Please enter a number between 1 and 3.";
+  public String PROMPT_VALID_CHOICE = "Invalid input. Please enter a number between 1 and 3.";
+  public String PROMPT_TITLE = "Enter the title of the quiz (cannot be empty and must be less than 100 characters): ";
+  public String PROMPT_QUESTION_TEXT = "Enter the question text (cannot be empty and must be less than 100 characters):";
+  public static final String SELECT_QUIZ = "Select a quiz to play :";
+  public static final String EXIT = "EXIT";
     
-    public static final String SCORE = "Total Score: ";
-    
-    public static final String OUT_OF = " out of ";
-    
-    public static final String NEXT_PREVIOUS_QUESTION = "Previous Question\t\t Next Question\t\t Submit";
-}
+  } 
