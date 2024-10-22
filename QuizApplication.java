4c2850b90b9315e9bdeef308d1d053ee17989bf1
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-22 12:09:51 +0530

    Update QuizApplication.java

--- QuizApplication.java
+++ QuizApplication.java
@@ -1,44 +1,51 @@
-package assignment_java_programming;
+package practice.java;
 
 /*
- * This is the class from where the program runs.
- * Created By : Sachin-Gupta
- * Date of Creation : 08/10/2024
+ * It provides the main menu options for the user to create or take a quiz.
+ * 
+ * Created By: Sachin-Gupta
+ * Date of Creation: 08/10/2024
  */
 
 import java.util.Scanner;
 
 public class QuizApplication {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Scanner input = new Scanner(System.in);
-        QuestionBank questionBank = new QuestionBank();
-        DefaultQuiz defaultQuiz = new DefaultQuiz();
-        Quiz quiz = new Quiz("");
+//        Scanner scanner = new Scanner(System.in); 
+        Scanner input = new Scanner(System.in); 
+        QuestionBank questionBank = new QuestionBank(); 
+        DefaultQuiz defaultQuiz = new DefaultQuiz(); 
+        Quiz quiz = new Quiz(""); 
 
+        // Loop to keep the application running until the user exits
         while (true) {
-            System.out.println(Constants.USER_CHOICE);
-            String choice = input.nextLine();
 
-            if (choice.equalsIgnoreCase("exit")) {
-                System.out.println(Constants.EXIT);
-                break;
+            System.out.println(Constants.USER_CHOICE); 
+            String choice = input.nextLine(); 
+            if (choice.equalsIgnoreCase(Constants.EXIT)) {
+                System.out.println(Message.EXIT); 
+                break; 
             }
 
             switch (choice) {
                 case "1":
-                    Creator creator = new Creator(questionBank, quiz);
-                    creator.create(scanner);
+                    // Option 1: Create a quiz
+                    Creator creator = new Creator(questionBank, quiz); 
+                    creator.create();
                     break;
+
                 case "2":
-                    Participant participant = new Participant(quiz, questionBank, defaultQuiz);
+                    // Option 2: Participate in a quiz
+                    Participant participant = new Participant(quiz, questionBank, defaultQuiz); 
                     participant.takeQuiz();
                     break;
+
                 default:
-                    System.out.println("Invalid Choice, please try again.");
+                    // Handle invalid choices
+                    System.out.println(Constants.INVALID_CHOICE);
                     break;
             }
         }
     }
 }
-
