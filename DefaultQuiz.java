41558dd93910ceaf2ab3a52f9077a670c4840821
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-22 12:11:21 +0530

    Update DefaultQuiz.java

--- DefaultQuiz.java
+++ DefaultQuiz.java
@@ -1,67 +1,65 @@
-package assignment_java_programming;
+package practice.java;
 
 /**
  * The DefaultQuiz class represents a default quiz with pre-defined questions.
- * It is used when no quizzes have been created by the user.
  * 
- * Creator : Uddeshya-Patidar
+ * Creator: Uddeshya Patidar
  * 
- * Date of Creation : 08/10/2024
+ * Date of Creation: 08/10/2024
  */
+public class DefaultQuiz {
 
-public class DefaultQuiz extends Quiz {
+    private String title; 
+    private Question[] questions;
+    private int questionCount; 
 
-	   private Question[] questions; 
-	    private int questionCount;
-    /** 
-     * Constructor for the DefaultQuiz class, initializing the quiz with a predefined title and loading the default questions.
-     */
+    //Constructor for the DefaultQuiz class, initializing the quiz with a title and predefined questions.
     public DefaultQuiz() {
-        super("Default Java Quiz"); 
-        questions = new Question[Constants.DEFAULT_QUESTIONS.length]; 
-        questionCount = 0; 
-        defaultQuestions();     }
+        this.title = "Basic Java Quiz"; 
+        this.questions = new Question[Message.DEFAULT_QUESTIONS.length]; 
+        this.questionCount = 0; 
+        initializeDefaultQuestions();
+    }
 
-    /** 
-     * Initializes the default questions, options, and answers for the quiz.
-     * It creates Question objects and adds them to the quiz.
-     */
-    private void defaultQuestions() {
-        String[] questionsData = Constants.DEFAULT_QUESTIONS;
-        String[][] optionsData = Constants.DEFAULT_OPTIONS;
-        String[] answersData = Constants.DEFAULT_ANSWERS;
+    //It creates Question objects and stores them in the questions array.
+    private void initializeDefaultQuestions() {
+        String[] questionsData = Message.DEFAULT_QUESTIONS; 
+        String[][] optionsData = Message.DEFAULT_OPTIONS; 
+        String[] answersData = Message.DEFAULT_ANSWERS; 
+        
+        // Iterate through the default question data and add them to the questions array
         for (int i = 0; i < questionsData.length; i++) {
-            Question question = new Question(questionsData[i], optionsData[i], answersData[i]); 
-            addQuestion(question);  
+            questions[questionCount++] = new Question(i + 1, questionsData[i], optionsData[i], answersData[i]);
         }
     }
-    
+
     /**
-     * This method is use to add questions to the array
+     * Gets a question by index.
+     * 
+     * @return The Question object at the specified index, or null if index is out of bounds.
      */
-    public void addQuestion(Question question) {
-        if (questionCount < questions.length) { 
-            questions[questionCount] = question; 
-            questionCount++; 
-        } else {
-            System.out.println("Cannot add more questions, quiz is full.");
-        }
-    }
-    
-    
     public Question getQuestion(int index) {
         if (index >= 0 && index < questionCount) {
             return questions[index]; 
         }
-        return null;
-    }
-    
-    public int getQuestionCount() {
-        return questionCount; 
-    }
-    
-    public String getTitle() {
-        return super.getTitle(); 
+        return null; 
     }
 
+    /**
+     * Gets the total number of questions.
+     * 
+     * @return The total number of questions in the quiz.
+     */
+    public int getQuestionCount() {
+        return questionCount; // Return the count of questions
+    }
+
+    /**
+     * Gets the title of the quiz.
+     * 
+     * @return The title of the quiz.
+     */
+    public String getTitle() {
+        return title; // Return the title of the quiz
+    }
 }
