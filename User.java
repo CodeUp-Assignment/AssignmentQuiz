511552812e1d5761c16b800d7cc62079e8c612c6
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-22 12:20:48 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,4 @@
-package assignment_java_programming;
+package practice.java;
 
 /*
  * The User class defines a blueprint for user objects with common properties (name and email) and an abstract method for performing role-specific actions in the quiz application.
@@ -21,6 +21,7 @@ public abstract class User {
     public String getName() {
         return name;
     }
+   
     // Set the name
     public void setName(String name) {
         this.name = name;
