5672204ae84519b9190c8ba415e0ace918822f29
Uddeshya Patidar <113989316+uddeshya64@users.noreply.github.com>
2024-10-22 12:09:00 +0530

    Update Participant.java

--- Participant.java
+++ Participant.java
@@ -1,4 +1,5 @@
-/***
+package practice.java;
+/*
  * This class allows participants to attempt the quiz. 
  * Users can answer the questions or navigate through the quiz.
  * 
@@ -6,20 +7,21 @@
  * 
  * Date - 08 October 2024
  */
-package project;
 
 import java.util.Scanner;
 
 public class Participant {
-    private QuestionBank questionBank;   
-    private DefaultQuiz defaultQuiz;     
+	private QuestionBank questionBank;
+    private DefaultQuiz defaultQuiz;
+    private Quiz quiz;
 
     /**
      *  Constructor to initialize QuestionBank and DefaultQuiz
      * @param questionBank
      * @param defaultQuiz
      */
-    public Participant(QuestionBank questionBank, DefaultQuiz defaultQuiz) {
+    public Participant(Quiz quiz,QuestionBank questionBank, DefaultQuiz defaultQuiz) {
+        this.quiz = quiz;
         this.questionBank = questionBank;
         this.defaultQuiz = defaultQuiz;
     }
@@ -30,13 +32,13 @@ public class Participant {
     public void takeQuiz() {
         Scanner input = new Scanner(System.in);
 
-        System.out.println(Constants.AVAILABLE_QUIZ);
+        System.out.println(Message.AVAILABLE_QUIZ);
         System.out.println("1. " + defaultQuiz.getTitle());  
 
-        System.out.print("Select a quiz to play (1): ");
+        System.out.print(Constants.SELECT_QUIZ);
         String selectedQuiz = input.nextLine();
 
-        if (selectedQuiz == 1) {
+        if (selectedQuiz.equals("1")) {
             play(defaultQuiz);  
         } else {
             System.out.println(Error.INVALID);
@@ -48,19 +50,19 @@ public class Participant {
      */
     public String[] play(DefaultQuiz defaultQuiz) {
         Scanner input = new Scanner(System.in);
-        System.out.println(Constants.WELCOME + defaultQuiz.getTitle());
+        System.out.println(Message.WELCOME + defaultQuiz.getTitle());
         int questionCount = defaultQuiz.getQuestionCount();
         String[] userAnswers = new String[questionCount];  
         System.out.println(Constants.PARTICIPANT_CHOICE);
         int index = 0;  
         while (index < questionCount) {
             Question questions = defaultQuiz.getQuestion(index); 
-            System.out.println(Constants.QUESTION_NO + (index + 1) + ": " + questions.getQuestionText());
+            System.out.println(Message.QUESTION_NO + (index + 1) + ": " + questions.getQuestionText());
             String[] options = questions.getOptions();
             for (int i = 0; i < options.length; i++) {
                 System.out.println((i + 1) + ") " + options[i]);
             }
-            System.out.println(Constants.NEXT_PREVIOUS_QUESTION);
+            System.out.println(Message.NEXT_PREVIOUS_QUESTION);
             String answer = input.nextLine();
             switch (answer) {
                 case "1":
@@ -77,7 +79,7 @@ public class Participant {
                     index = index + 1;
                     break;
                 case "s":  
-                	System.out.println(Constants.FINISH);
+                	System.out.println(Message.FINISH);
                     return userAnswers;
                 default:
                     System.out.println(Error.INVALID);
