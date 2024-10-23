8dca919f439c871f811100331628310d612a6dfb
Sachin Gupta <120103804+SachinXCode313@users.noreply.github.com>
2024-10-23 14:12:14 +0530

    Update QuizApplication.java

--- QuizApplication.java
+++ QuizApplication.java
@@ -1,8 +1,8 @@
-package practice.java;
+package assignment_java_programming;
 
 /*
  * It provides the main menu options for the user to create or take a quiz.
- * 
+ *
  * Created By: Sachin-Gupta
  * Date of Creation: 08/10/2024
  */
@@ -12,38 +12,41 @@ import java.util.Scanner;
 public class QuizApplication {
 
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in); 
-        Scanner input = new Scanner(System.in); 
-        QuestionBank questionBank = new QuestionBank(); 
-        DefaultQuiz defaultQuiz = new DefaultQuiz(); 
-        Quiz quiz = new Quiz(""); 
+        Scanner input = new Scanner(System.in);
+        QuestionBank questionBank = new QuestionBank();
+        DefaultQuiz defaultQuiz = new DefaultQuiz();
+        Quiz quiz = null;
 
         // Loop to keep the application running until the user exits
         while (true) {
-
-            System.out.println(Constants.USER_CHOICE); 
-            String choice = input.nextLine(); 
-            if (choice.equalsIgnoreCase(Constants.EXIT)) {
-                System.out.println(Message.EXIT); 
-                break; 
+            System.out.println("Enter your name: ");
+            String name = input.nextLine();
+            System.out.println(Constant.USER_CHOICE);
+            String choice = input.nextLine();
+            if (choice.equalsIgnoreCase(Constant.EXIT)) {
+                System.out.println(Message.EXIT);
+                break;
             }
-
+            User user;
             switch (choice) {
                 case "1":
                     // Option 1: Create a quiz
-                    Creator creator = new Creator(questionBank, quiz); 
+                    Creator creator = new Creator(name, questionBank);
                     creator.create();
+                    quiz = creator.getQuiz();
                     break;
-
                 case "2":
                     // Option 2: Participate in a quiz
-                    Participant participant = new Participant(quiz, questionBank, defaultQuiz); 
-                    participant.takeQuiz();
+                    Participant participant = new Participant(name, defaultQuiz);
+                    participant.takeQuiz(quiz);
+                    break;
+
+                case "exit":
                     break;
 
                 default:
                     // Handle invalid choices
-                    System.out.println(Constants.INVALID_CHOICE);
+                    System.out.println(Constant.INVALID_CHOICE);
                     break;
             }
         }
