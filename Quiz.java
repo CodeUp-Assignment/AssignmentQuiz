7c453fb2eb32b10e21e760a687f2f9eb50733617
Sachin Gupta <120103804+SachinXCode313@users.noreply.github.com>
2024-10-23 14:08:50 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,27 +1,27 @@
-package practice.java;
+package assignment_java_programming;
+
 /***
  * The Quiz class represents a quiz with a title and a collection of questions.
- * 
+ *
  * Owner : Avadhi-Singhal
- * 
+ *
  * Date of Creation : 10/10/2024
  */
 
 public class Quiz {
-	Constants constant = new Constants();
-	
-    private String title; 
-    private Question[] questions; 
-    private int currentQuestionCount; 
-    
+    Constant constant = new Constant();
+    private String title;
+    private Question[] questions;
+    private int currentQuestionCount;
+
     /**
      * Constructor for initializing the quiz with a title.
-     * 
+     *
      * @param title The title of the quiz.
      */
     public Quiz(String title) {
         this.title = title;
-        this.questions = new Question[constant.DEFAULT_QUESTION_NUMBERS]; 
+        this.questions = new Question[constant.DEFAULT_QUESTION_NUMBERS];
         this.currentQuestionCount = 0;
     }
 
@@ -37,29 +37,37 @@ public class Quiz {
 
     /**
      * Adds a question to the quiz.
-     * 
+     *
      * @param question The question to be added.
      */
     public void addQuestion(Question question) {
         if (currentQuestionCount >= questions.length) {
             resizeQuiz();
         }
-        questions[currentQuestionCount++] = question; 
+        questions[currentQuestionCount++] = question;
     }
 
     /**
      * Resizes the question array to accommodate more questions.
      */
     private void resizeQuiz() {
-        Question[] newQuestions = new Question[questions.length * 2]; 
+        Question[] newQuestions = new Question[questions.length * 2];
         for (int i = 0; i < questions.length; i++) {
-            newQuestions[i] = questions[i]; 
+            newQuestions[i] = questions[i];
         }
-        questions = newQuestions; 
+        questions = newQuestions;
     }
-    
+
     public String getTitle() {
-    	return title;
+        return title;
+    }
+
+    public Question[] getQuestions() {
+        return questions;
+    }
+
+    public int getCurrentQuestionCount() {
+        return currentQuestionCount;
     }
 }
 
