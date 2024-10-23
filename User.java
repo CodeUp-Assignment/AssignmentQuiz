5ececdfbe622265f6e086f72af70ee5192b3d165
Sachin Gupta <120103804+SachinXCode313@users.noreply.github.com>
2024-10-23 14:12:35 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,4 @@
-package practice.java;
+package assignment_java_programming;
 
 /*
  * The User class defines a blueprint for user objects with common properties (name and email) and an abstract method for performing role-specific actions in the quiz application.
@@ -6,34 +6,21 @@ package practice.java;
  * Date of Creation : 08/10/2024
  */
 
-import java.util.Scanner;
 public abstract class User {
     private String name;
-    private String email;
 
     // Constructor to initialize name and email
-    public User(String name, String email) {
+    public User(String name) {
         this.name = name;
-        this.email = email;
     }
 
     // Get the name
     public String getName() {
         return name;
     }
-   
+
     // Set the name
     public void setName(String name) {
         this.name = name;
     }
-    // Get the email
-    public String getEmail() {
-        return email;
-    }
-    // Set the email
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    // performing role-specific actions
-    public abstract void performAction(Scanner input);
 }
